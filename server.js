0f8fe47eb062c1149dcd930681a9c99e3b541850
kerloshany3 <kerlostablet@gmail.com>
2025-07-24 06:58:05 +0300

    Fix Endpoints

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ const statsRoutes = require("./routes/statsRoutes");
 const bookRoutes = require("./routes/bookRoutes");
 const bookOrderRoutes = require("./routes/bookOrderRoutes");
 const courseAnalyticsRoutes = require("./routes/courseAnalyticsRoutes");
+const courseWithEnrollmentRoutes = require("./routes/courseWithEnrollmentRoutes");
 
 dotenv.config();
 const app = express();
